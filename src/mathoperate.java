6447959dfd49c073657d991d2bcd0304de0d7a28
Riyaz-commits <skriyazbasha91@gmail.com>
2022-08-20 23:25:41 +0530

    second commit

--- src/mathoperate.java
+++ src/mathoperate.java
@@ -12,6 +12,7 @@ public class mathoperate {
         System.out.println(calculate(add));
         System.out.println(calculate(sub));
         System.out.println(calculate(div));
+        System.out.println("here it is ");
         }
     public static int calculate(math m)
     {
